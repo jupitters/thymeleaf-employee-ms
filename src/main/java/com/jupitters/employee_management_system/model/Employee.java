4aab8cf82814f5aa898f32fa351a4cbcfd088268
jupitters <sampaiobale@gmail.com>
2026-02-04 15:27:05 -0300

    employee model

--- src/main/java/com/jupitters/employee_management_system/model/Employee.java
+++ src/main/java/com/jupitters/employee_management_system/model/Employee.java
@@ -1,9 +1,19 @@
 package com.jupitters.employee_management_system.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
