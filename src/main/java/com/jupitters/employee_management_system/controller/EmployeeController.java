f3d9f105b217dffe7a160a0804e91a6166d7f396
jupitters <sampaiobale@gmail.com>
2026-02-04 15:38:08 -0300

    employee controller

--- src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import com.jupitters.employee_management_system.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
 
+    @GetMapping("/")
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
