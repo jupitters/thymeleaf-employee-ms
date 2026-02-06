ba80299396b473170537bfaeb601becc2db4045a
jupitters <sampaiobale@gmail.com>
2026-02-06 11:20:25 -0300

    new employee controller

--- src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,5 +24,10 @@ public class EmployeeController {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "new_employee";
+    }
+
+    public String saveEmployee(@ModelAttribute("employee") Employee employee, Model model){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
