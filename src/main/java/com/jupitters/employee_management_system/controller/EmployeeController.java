09bc3c2d7dc2bdcb433e7c8831854f406348dec3
jupitters <sampaiobale@gmail.com>
2026-02-06 11:20:40 -0300

    new employee controller

--- src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/jupitters/employee_management_system/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +27,7 @@ public class EmployeeController {
         return "new_employee";
     }
 
+    @PostMapping("/saveEmployee")
     public String saveEmployee(@ModelAttribute("employee") Employee employee, Model model){
         employeeService.saveEmployee(employee);
         return "redirect:/";
