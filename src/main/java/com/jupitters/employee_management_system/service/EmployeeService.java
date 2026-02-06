8a12bf7b66c9b40d0569fdbed8cd802e00451396
jupitters <sampaiobale@gmail.com>
2026-02-06 13:03:48 -0300

    employee delete service

--- src/main/java/com/jupitters/employee_management_system/service/EmployeeService.java
+++ src/main/java/com/jupitters/employee_management_system/service/EmployeeService.java
@@ -8,4 +8,5 @@ public interface EmployeeService {
     List<Employee> getAllEmployees();
     void saveEmployee(Employee employee);
     Employee getEmployeeById(Long id);
+    void deleteEmployee(Long id);
 }
