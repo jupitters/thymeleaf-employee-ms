4cfe5a667360e24da0a47ff097daabe7fb96f206
jupitters <sampaiobale@gmail.com>
2026-02-07 13:36:23 -0300

    model

--- src/main/java/com/jupitters/employee_management_system/model/Employee.java
+++ src/main/java/com/jupitters/employee_management_system/model/Employee.java
@@ -9,16 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
+
 }
